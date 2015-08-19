7855928713931053cd62cf15a7ed44e14d03f463
Aziz <aziz.saboor@nexchange.com>
2015-08-19 18:07:40 +0800

    - added a list using angular's ng-repeat - and ordered it using 'orderBy' - and provided a way for it to be searched/filtered using 'filter' - additionally provided a way to add a hard coded post to the current list of posts

--- app.js
+++ app.js
@@ -5,22 +5,84 @@
     Aziz | 18 Aug 2015
  */
 
+
+//////////////////////////////////////////////////
+// Modules
+//////////////////////////////////////////////////
+
 // angular modules are basically mini applications 
 var app = angular.module( 'crowdlines', [] );
 
-// definition of the 'MainCtrl' controller, which is referenced within index.html
+
+
+//////////////////////////////////////////////////
+// Controllers
+//////////////////////////////////////////////////
+
 app.controller
 ( 
-    'MainCtrl',
+    'mainCtrl', 
+    function( $scope )
+    {
+        $scope.test = "salam dunya";
+        $scope.posts =
+        [
+            { title: 'post 1', upvotes: 5 },
+            { title: 'post 2', upvotes: 2 },
+            { title: 'post 3', upvotes: 15 },
+            { title: 'post 4', upvotes: 9 },
+            { title: 'post 5', upvotes: 4 }
+        ];
+        
+        // everytime 'addPost' is accessed, it will invoke the anonymous function
+        // that will cause a new post to be appended to the array
+        $scope.addPost = function()
+        {
+            $scope.posts.push( { title: 'A new post!', upvotes: 0 } );
+        };
+    } 
+);
+
+
+/*// definition of the 'mainCtrl' controller, which is referenced within index.html
+app.controller
+( 
+    'mainCtrl',
     [
         '$scope',
         
         // the $scope variable acts like a bridge between the view and the controller
-        // anything that should be accessible to the view can be binded to the $scope
+        // anything that should be accessible to the view should be binded to the $scope
         function( $scope )
         {
-            $scope.test = 'Hello World';
+            $scope.test = "salam dunya";
+            $scope.posts =
+            [
+                { title: 'post 1', upvotes: 5 },
+                { title: 'post 2', upvotes: 2 },
+                { title: 'post 3', upvotes: 15 },
+                { title: 'post 4', upvotes: 9 },
+                { title: 'post 5', upvotes: 4 }
+            ];
         }
     ]
+);*/
+
+
+
+//////////////////////////////////////////////////
+// Filters
+//////////////////////////////////////////////////
+
+app.filter
+( 
+    'capitalise', 
+    function()
+    {
+        return function( text )
+        {
+            return text.toUpperCase();
+        };
+    } 
 );
 
