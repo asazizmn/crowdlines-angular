903c9a98659d8562814b42741e2356a1c760e552
Aziz <aziz.saboor@nexchange.com>
2015-08-20 11:09:24 +0800

    - added the ability to added custom post titles - and also changed javasript string literals to use double quotes instead (following java conventions)

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 //////////////////////////////////////////////////
 
 // angular modules are basically mini applications 
-var app = angular.module( 'crowdlines', [] );
+var app = angular.module( "crowdlines", [] );
 
 
 
@@ -19,37 +19,46 @@ var app = angular.module( 'crowdlines', [] );
 // Controllers
 //////////////////////////////////////////////////
 
+// definition of the 'mainCtrl' controller, which is referenced within index.html
 app.controller
 ( 
-    'mainCtrl', 
+    "mainCtrl", 
     function( $scope )
     {
         $scope.test = "salam dunya";
         $scope.posts =
         [
-            { title: 'post 1', upvotes: 5 },
-            { title: 'post 2', upvotes: 2 },
-            { title: 'post 3', upvotes: 15 },
-            { title: 'post 4', upvotes: 9 },
-            { title: 'post 5', upvotes: 4 }
+            { title: "post 1", upvotes: 5 },
+            { title: "post 2", upvotes: 2 },
+            { title: "post 3", upvotes: 15 },
+            { title: "post 4", upvotes: 9 },
+            { title: "post 5", upvotes: 4 }
         ];
         
         // everytime 'addPost' is accessed, it will invoke the anonymous function
         // that will cause a new post to be appended to the array
         $scope.addPost = function()
-        {
-            $scope.posts.push( { title: 'A new post!', upvotes: 0 } );
+        {   
+            // this simple adds a new post with hardcoded values
+            // $scope.posts.push( { title: 'A new post!', upvotes: 0 } );
+            
+            // $scope.title allows the form bounded value to be accessed here
+            $scope.posts.push( { title: $scope.title, upvotes: 0 } );
+            
+            // and this will allow the bound title input to be cleared at this point
+            // please note that we have already captured the title above
+            $scope.title = "";
         };
     } 
 );
 
 
-/*// definition of the 'mainCtrl' controller, which is referenced within index.html
+/*
 app.controller
 ( 
-    'mainCtrl',
+    "mainCtrl",
     [
-        '$scope',
+        "$scope",
         
         // the $scope variable acts like a bridge between the view and the controller
         // anything that should be accessible to the view should be binded to the $scope
@@ -58,11 +67,11 @@ app.controller
             $scope.test = "salam dunya";
             $scope.posts =
             [
-                { title: 'post 1', upvotes: 5 },
-                { title: 'post 2', upvotes: 2 },
-                { title: 'post 3', upvotes: 15 },
-                { title: 'post 4', upvotes: 9 },
-                { title: 'post 5', upvotes: 4 }
+                { title: "post 1", upvotes: 5 },
+                { title: "post 2", upvotes: 2 },
+                { title: "post 3", upvotes: 15 },
+                { title: "post 4", upvotes: 9 },
+                { title: "post 5", upvotes: 4 }
             ];
         }
     ]
@@ -76,7 +85,7 @@ app.controller
 
 app.filter
 ( 
-    'capitalise', 
+    "capitalise", 
     function()
     {
         return function( text )
