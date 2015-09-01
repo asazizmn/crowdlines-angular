3f5806f41087b2cfe6caf8799f293f92f988ea58
Aziz <aziz.saboor@nexchange.com>
2015-09-01 12:06:11 +0800

    - added the ability to upvote a comment

--- app.js
+++ app.js
@@ -249,6 +249,12 @@ app.controller
             // to clear field after submission
             $scope.comment = "";
         };
+        
+        
+        $scope.upTheVotes = function( comment )
+        {
+            comment.upvotes += 1;
+        };
     }
 );
 
